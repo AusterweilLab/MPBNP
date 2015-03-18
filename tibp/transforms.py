d32097b709e0a3a231e791ed3e23ef0e11d3b5b6
Ting Qian <tqian86@gmail.com>
2015-03-18 10:01:16 -0400

    Started creating transformation functions for tIBP

---
+++ tibp/transforms.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+
+def v_translation(f_img, f_img_width, distance):
+    """Translate a feature image vertically by distance (+ 
+    indicates down, - indicates up).
+    """
+    pass
+
+def h_translation(f_img, f_img_width, distance):
+    """Translate a feature image horizontally by distance (+ 
+    indicates right, - indicates left).
+    """
+    pass
+
+def scale(f_img, f_img_width, percent):
+    """Scale a feature image by holding the top left corner
+    of the image constant.
+    """
+    pass
+    
